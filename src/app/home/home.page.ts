450452bb3b7a9e18b1de177ca241038ec6997be8
Anuchit <anuchit.b@origin55.com>
2022-07-27 22:18:47 +0700

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,154 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { ToastController, LoadingController, Platform } from '@ionic/angular';
+import jsQR from 'jsqr';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
-export class HomePage {
+export class HomePage implements AfterViewInit {
+  @ViewChild('video', { static: false }) video: ElementRef;
+  @ViewChild('canvas', { static: false }) canvas: ElementRef;
+  @ViewChild('fileinput', { static: false }) fileinput: ElementRef;
 
-  constructor() {}
+  canvasElement: any;
+  videoElement: any;
+  canvasContext: any;
+  scanActive = false;
+  scanResult = null;
+  loading: HTMLIonLoadingElement = null;
 
+  constructor(
+    private toastCtrl: ToastController,
+    private loadingCtrl: LoadingController,
+    private plt: Platform
+  ) {
+    const isInStandaloneMode = () => false;
+      //'standalone' in window.navigator && window.navigator['standalone'];
+    if (this.plt.is('ios') && isInStandaloneMode()) {
+      console.log('I am a an iOS PWA!');
+      // E.g. hide the scan functionality!
+    }
+  }
+
+  ngAfterViewInit() {
+    this.canvasElement = this.canvas.nativeElement;
+    this.canvasContext = this.canvasElement.getContext('2d');
+    this.videoElement = this.video.nativeElement;
+  }
+
+  // Helper functions
+  async showQrToast() {
+    const toast = await this.toastCtrl.create({
+      message: `Open ${this.scanResult}?`,
+      position: 'top',
+      buttons: [
+        {
+          text: 'Open',
+          handler: () => {
+            window.open(this.scanResult, '_system', 'location=yes');
+          }
+        }
+      ]
+    });
+    toast.present();
+  }
+
+  reset() {
+    this.scanResult = null;
+  }
+
+  stopScan() {
+    this.scanActive = false;
+  }
+
+  async startScan() {
+    // Not working on iOS standalone mode!
+    const stream = await navigator.mediaDevices.getUserMedia({
+      video: { facingMode: 'environment' }
+    });
+
+    this.videoElement.srcObject = stream;
+    // Required for Safari
+    this.videoElement.setAttribute('playsinline', true);
+
+    this.loading = await this.loadingCtrl.create({});
+    await this.loading.present();
+
+    this.videoElement.play();
+    requestAnimationFrame(this.scan.bind(this));
+  }
+
+  async scan() {
+    if (this.videoElement.readyState === this.videoElement.HAVE_ENOUGH_DATA) {
+      if (this.loading) {
+        await this.loading.dismiss();
+        this.loading = null;
+        this.scanActive = true;
+      }
+
+      this.canvasElement.height = this.videoElement.videoHeight;
+      this.canvasElement.width = this.videoElement.videoWidth;
+
+      this.canvasContext.drawImage(
+        this.videoElement,
+        0,
+        0,
+        this.canvasElement.width,
+        this.canvasElement.height
+      );
+      const imageData = this.canvasContext.getImageData(
+        0,
+        0,
+        this.canvasElement.width,
+        this.canvasElement.height
+      );
+      const code = jsQR(imageData.data, imageData.width, imageData.height, {
+        inversionAttempts: 'dontInvert'
+      });
+
+      if (code) {
+        this.scanActive = false;
+        this.scanResult = code.data;
+        this.showQrToast();
+      } else {
+        if (this.scanActive) {
+          requestAnimationFrame(this.scan.bind(this));
+        }
+      }
+    } else {
+      requestAnimationFrame(this.scan.bind(this));
+    }
+  }
+
+  captureImage() {
+    this.fileinput.nativeElement.click();
+  }
+
+  handleFile(event: Event) {
+    const element = event.currentTarget as HTMLInputElement;
+    const fileList: FileList | null = element.files;
+    const file = fileList.item(0);
+
+    const img = new Image();
+    img.onload = () => {
+      this.canvasContext.drawImage(img, 0, 0, this.canvasElement.width, this.canvasElement.height);
+      const imageData = this.canvasContext.getImageData(
+        0,
+        0,
+        this.canvasElement.width,
+        this.canvasElement.height
+      );
+      const code = jsQR(imageData.data, imageData.width, imageData.height, {
+        inversionAttempts: 'dontInvert'
+      });
+
+      if (code) {
+        this.scanResult = code.data;
+        this.showQrToast();
+      }
+    };
+    img.src = URL.createObjectURL(file);
+  }
 }
